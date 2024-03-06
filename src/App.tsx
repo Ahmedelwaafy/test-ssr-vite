ffa0e21e69dab9b1719ccc4cf164c594d4697ffd
Ahmedelwaafy <ahmedelwaafy@gmail.com>
2024-03-06 17:23:21 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sit nam
+        cumque tenetur officiis fugiat aut, aspernatur fugit autem porro rerum
+        consectetur, debitis nostrum iste doloremque? Iste porro labore, iure
+        optio vero tempora?
+      </div>
+      <img
+        style={{ maxWidth: "100%" }}
+        src="https://images.unsplash.com/photo-1709633694594-30f42ddbf669?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+    </>
+  );
+}
+
+export default App;
